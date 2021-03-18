caddaea430f97443d0ec3b3332bdeb3ef8cdfb38
Admin <volodimir.martuniyk@gmail.com>
2021-03-18 11:22:30 +0200

    Added Alpine.js to SearchDropdown for better UX

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>Laracasts Video Games</title>
     <link rel="stylesheet" href="/css/main.css">
     @livewireStyles
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body class="bg-gray-900 text-white">
     <header class="border-b border-gray-800">
