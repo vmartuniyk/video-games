08b78925fe5f3019f6763a73d11c7e90c3fdffb8
Admin <volodimir.martuniyk@gmail.com>
2021-03-19 16:03:38 +0200

    Update readme file

--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -27,7 +27,7 @@
     @empty
         @foreach(range(1,3) as $game)
         <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
-                <div class="relative flex-none">
+                <div class="relative flex">
                     <div class="bg-gray-700 w-32 lg:48 h-40 lg:h-56"></div>
                 </div>
                 <div class="ml-6 lg:ml-12">
