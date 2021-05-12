b6bc1deac8efe7ce6d81f0c7e797e4f8ecaea9c6
vmartuniyk <volodimir.martuniyk@gmail.com>
2021-05-12 14:14:51 +0300

    Delete cache folder bootstrap/cache

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'coming-soon' => 'App\\Http\\Livewire\\ComingSoon',
+  'most-anticipated' => 'App\\Http\\Livewire\\MostAnticipated',
+  'popular-games' => 'App\\Http\\Livewire\\PopularGames',
+  'recently-reviewed' => 'App\\Http\\Livewire\\RecentlyReviewed',
+  'search-dropdown' => 'App\\Http\\Livewire\\SearchDropdown',
+);
